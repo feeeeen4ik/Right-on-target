2640f777a592a33007bf61ac74a17c7a5da39527
feeeeeen4ik <feeeeen4ik1994@gmail.com>
2024-11-06 19:58:48 +0500

    Initial Commit

---
+++ Right on target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right on target
+//
+//  Created by Феликс Антонович on 06.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
